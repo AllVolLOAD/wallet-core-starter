3c06b8e5920e7354b921a359e9e88ac5e696204c
notebook16gb <vsevo.on@gmail.com>
2025-10-20 14:28:23 +0300

    try

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -6,7 +6,7 @@ import org.example.utils.Printer
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    val second = ne    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     val message = "Hello, " + name + "!"
     val printer = Printer(message)
